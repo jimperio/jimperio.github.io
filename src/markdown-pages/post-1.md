ffaa5bbd06f73ba61aeee6dbe55bc496629ce161
Jomel Imperio <jimperio@gmail.com>
2020-09-06 11:50:49 +0800

    Initial commit: home page, basic blog post from markdown

---
+++ src/markdown-pages/post-1.md
@@ -0,0 +1,6 @@
+---
+slug: "/blog/my-first-post"
+date: "2020-09-06"
+title: "My first blog post"
+---
+Hello, everyone.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,24 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1>hello, I'm Jomel Imperio! &#x1F44B;</h1>
+      <h2>what I do:</h2>
+      <ul>
+        <li>
+          write - <Link to="/blog">all blog posts</Link>
+        </li>
+        <li>read</li>
+        <li>
+          draw -{" "}
+          <a href="https://instagram.com/jomel.draws/">IG: jomel.draws</a>
+        </li>
+        <li>
+          make software - CTO at <a href="https://www.insynchq.com/">Insync</a>
+        </li>
+      </ul>
+    </div>
+  )
 }
